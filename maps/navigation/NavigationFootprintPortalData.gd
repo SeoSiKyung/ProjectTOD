55cc3c25cd33233653c7636449873cd4a07d700b
yukiys <yukihw1218@gmail.com>
2026-08-29 17:40:50 +0900

    Improve A* performance with portal routing and cached navigation data

---
+++ maps/navigation/NavigationFootprintPortalData.gd
@@ -0,0 +1,8 @@
+class_name NavigationFootprintPortalData
+extends Resource
+
+@export var portalId: int = -1
+@export var traversable: bool = false
+
+# 진입로
+@export var anchors: PackedVector2Array = PackedVector2Array()
